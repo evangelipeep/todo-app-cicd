1ad75ba5698ceb877db3d901bd374fe3525cbf6b
evangelipeep <evangelipeep@gmail.com>
2025-10-17 20:25:29 +0300

    className-update-1

--- cypress/e2e/todo.cy.ts
+++ cypress/e2e/todo.cy.ts
@@ -4,13 +4,13 @@ describe("Todo App", () => {
   });
 
   it("Добавление задачи", () => {
-    cy.get("input").type("Первая задача");
+    cy.get(".todo-input").type("Первая задача");
     cy.contains("Добавить").click();
     cy.contains("Первая задача").should("exist");
   });
 
   it("Отметка задачи выполненной", () => {
-    cy.get("input").type("Вторая задача");
+    cy.get(".todo-input").type("Вторая задача");
     cy.contains("Добавить").click();
     cy.get("input[type='checkbox']").click();
     cy.contains("Вторая задача").should(
@@ -21,7 +21,7 @@ describe("Todo App", () => {
   });
 
   it("Удаление задачи", () => {
-    cy.get("input").type("Третья задача");
+    cy.get(".todo-input").type("Третья задача");
     cy.contains("Добавить").click();
     cy.contains("Удалить").click();
     cy.contains("Третья задача").should("not.exist");
